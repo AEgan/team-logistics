de678a822b57bafd9b604ee549666d7b2409d7da
Alex Egan <aegan@andrew.cmu.edu>
2013-11-22 02:31:35 -0500

    added maps created by node module called googlemaps. Really easy. Changed it from direct API call to this to make it cleaner, more clear

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var users = require('../models/user.js');
-
+var util = require('util');
+var gm = require('googlemaps');
 /*
  * Insert a new user
  */
@@ -58,7 +59,20 @@ exports.destroy = function(req, res) {
 
 exports.show = function(req, res) {
 	users.show(req.params.username, function(model) {
-		var mapString = "http://maps.googleapis.com/maps/api/staticmap?center="+model.latitude+","+model.longitude+"&zoom=12&size=400x400&sensor=false";
-		res.render('userPage', {title: "title test", header: "hi", obj: model, map: mapString});
+		var address = "" + model.street + " " + model.city + " " + model.state;
+		var markers = [
+			{'location': address,
+			'color': 'red',
+			'shadow': false,
+			'icon': "http://chart.apis.google.com/chart?chst=d_map_pin_icon&chld=cafe%7C996600"
+			 }
+		];
+		var styles = [
+    	{ 'feature': 'road', 'element': 'all', 'rules': 
+      	  { 'hue': '0x00ff00' }
+    	}
+		];
+		var mapStr = gm.staticMap(address, 15, '500x400', false, false, 'roadmap', markers, styles);
+		res.render('userPage', {title: "title test", header: "hi", obj: model, map: mapStr});
 	});
 }
