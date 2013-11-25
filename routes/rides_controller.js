a59c425cec6b5628848d14a9f2a7037d2163f781
Alex Egan <aegan@andrew.cmu.edu>
2013-11-24 23:54:32 -0500

    edited controller actions to have proper redirects after creating an event

--- routes/rides_controller.js
+++ routes/rides_controller.js
@@ -2,6 +2,7 @@ var teams = require('../models/team.js');
 var async = require("async");
 var users = require('../models/user.js');
 var rides = require('../models/ride.js');
+var gm = require('googlemaps');
 
 exports.index = function(req, res) {
 	res.render('rideIndex');
@@ -15,7 +16,9 @@ exports.insert = function(req, res) {
 	teams.find(req.params.name, function(docs) {
 		var teamID = docs[0]._id;
 		rides.insert(teamID, req.params.event, req.body.driverID, req.body.time, req.body.spots, function(result) {
-			res.render('rideIndex');
+			teams.showEvent(req.params.name, req.params.event, function(team, theEvent) {
+				res.redirect('/teams/' + team.name + '/' + theEvent.name);
+			});
 		});
 	});
 }
