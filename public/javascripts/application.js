4f37251b0c573d1f233efcd4455da382dfd58d58
Alex Egan <aegan@andrew.cmu.edu>
2013-12-04 00:55:28 -0500

    added button and listener for joining rides now need to do backend

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
 $(document).ready(function(){
+	var joinRideButtons = $('.joinRideButton');
+	$.each(joinRideButtons, function(index, value) {
+		$(value).click(function(){
+			alert("here");
+		});
+	});
 });
